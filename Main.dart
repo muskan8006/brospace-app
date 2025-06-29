645970323bf57c812efe4126a01cc632cedda7bb
Muskan <aboutmuskann@gmail.com>
2025-06-29 10:44:09 +0530

    Update Main.dart

--- Main.dart
+++ Main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const BroSpaceApp());
+}
+
+class BroSpaceApp extends StatelessWidget {
+  const BroSpaceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BroSpace',
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
