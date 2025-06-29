13a3d17d748d6a09144188b5b9db5f4280939387
Muskan <aboutmuskann@gmail.com>
2025-06-29 10:48:11 +0530

    Create Mood_selector.dart

---
+++ Mood_selector.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MoodSelector extends StatelessWidget {
+  const MoodSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Select Your Mood')),
+      body: Center(child: Text('Mood Selector Coming Soon')),
+    );
+  }
+}
